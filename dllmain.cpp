9e99af3c3a1e0c22eb0f6cc7d769f39a4ec4bfc7
GH-iPower <34873714+GH-iPower@users.noreply.github.com>
2020-01-22 03:50:20 -0300

    code formatting

--- dllmain.cpp
+++ dllmain.cpp
@@ -5,18 +5,18 @@ DWORD __stdcall thread( LPVOID )
 {
 	AllocConsole();
 	FILE* f_ptr = nullptr;
-	freopen_s(&f_ptr, "CONOUT$", "w", stdout);
-	SetConsoleTitle(TEXT("Lazysight"));
+	freopen_s( &f_ptr, "CONOUT$", "w", stdout );
+	SetConsoleTitle( TEXT( "Lazysight" ) );
 
-	const auto base = reinterpret_cast<uintptr_t>( GetModuleHandleA( nullptr ) );
+	const auto base = reinterpret_cast< uintptr_t >( GetModuleHandleA( nullptr ) );
 
 	// g_render A1 ? ? ? ? 8B 88 ? ? ? ? 8B 46 1C 8B 56 0C 8B 78 54 
 	auto g_render = 0;
 
-	while ( !( g_render = *reinterpret_cast<uintptr_t*>( base + 0xA89198 ) ) )
-		Sleep(200);
+	while ( !( g_render = *reinterpret_cast< uintptr_t* >( base + 0xA89198 ) ) )
+		Sleep( 200 );
 
-	const auto p_device = **reinterpret_cast<uintptr_t***>( g_render + 0x20 );
+	const auto p_device = **reinterpret_cast< uintptr_t*** >( g_render + 0x20 );
 
 	const auto present = p_device[17];
 
@@ -26,7 +26,7 @@ DWORD __stdcall thread( LPVOID )
 		return 0;
 	}
 
-	if ( MH_CreateHook( reinterpret_cast<LPVOID>( present ) , &hk_present , reinterpret_cast<LPVOID*>( &o_present ) ) != MH_OK )
+	if ( MH_CreateHook( reinterpret_cast< LPVOID >( present ) , &hk_present , reinterpret_cast< LPVOID* >( &o_present ) ) != MH_OK )
 	{
 		printf( "[Lazysight] Failed to create hook at Present\n" );
 		return 0;
@@ -44,7 +44,7 @@ DWORD __stdcall thread( LPVOID )
 bool __stdcall DllMain( HINSTANCE, DWORD reason, LPVOID )
 {
 	if ( reason == DLL_PROCESS_ATTACH )
-		CreateThread(nullptr, 0, thread, nullptr, 0, nullptr);
+		CreateThread( nullptr, 0, thread, nullptr, 0, nullptr );
 
 	return true;
 }
--- hooks/hooks.h
+++ hooks/hooks.h
@@ -7,35 +7,35 @@
 
 WNDPROC o_wndproc = nullptr;
 bool b_render_menu = false;
-LRESULT wnd_proc(const HWND hwnd, const UINT u_msg, const WPARAM w_param, const LPARAM l_param)
-{
-	extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND, UINT, WPARAM, LPARAM);
 
-	if (u_msg == WM_KEYDOWN && w_param == VK_INSERT)
+LRESULT wnd_proc( const HWND hwnd, const UINT u_msg, const WPARAM w_param, const LPARAM l_param )
+{
+	extern LRESULT ImGui_ImplWin32_WndProcHandler( HWND, UINT, WPARAM, LPARAM );
+
+	if ( u_msg == WM_KEYDOWN && w_param == VK_INSERT )
 		b_render_menu = !b_render_menu;
 
-	if ( ImGui_ImplWin32_WndProcHandler(hwnd, u_msg, w_param, l_param) )
+	if ( ImGui_ImplWin32_WndProcHandler( hwnd, u_msg, w_param, l_param ) )
 		return 1l;
 
-	return CallWindowProc( o_wndproc , hwnd, u_msg, w_param, l_param );
+	return CallWindowProc( o_wndproc, hwnd, u_msg, w_param, l_param );
 }
 
 
-using fn_present = long(__stdcall*)(IDirect3DDevice9*, const RECT*, const RECT*, HWND, const RGNDATA*);
+using fn_present = long( __stdcall* )( IDirect3DDevice9*, const RECT*, const RECT*, HWND, const RGNDATA* );
 fn_present o_present = nullptr;
 
 long __stdcall hk_present( IDirect3DDevice9* p_device, const RECT* p_src_rect, const RECT* p_dest_rect, HWND h_dest_window, const RGNDATA* p_dirty_region )
 {
 	static std::once_flag o_flag;
-	std::call_once(o_flag, [p_device]() 
+	std::call_once( o_flag, [p_device]()
 	{
 		const auto s_hwnd = FindWindow( nullptr, TEXT( "ironsight - 1.4.383.63755" ) );
-		o_wndproc = reinterpret_cast<WNDPROC>( SetWindowLongPtr( s_hwnd, GWLP_WNDPROC, LONG_PTR( wnd_proc ) ) );
+		o_wndproc = reinterpret_cast< WNDPROC >( SetWindowLongPtr( s_hwnd, GWLP_WNDPROC, LONG_PTR( wnd_proc ) ) );
 		ImGui::CreateContext();
 		ImGui_ImplWin32_Init( s_hwnd );
 		ImGui_ImplDX9_Init( p_device );
-
-	});
+	} );
 
 	ImGui_ImplDX9_NewFrame();
 	ImGui_ImplWin32_NewFrame();
@@ -43,16 +43,16 @@ long __stdcall hk_present( IDirect3DDevice9* p_device, const RECT* p_src_rect, c
 
 	if ( b_render_menu )
 	{
-		if ( ImGui::Begin("Lazysight", nullptr, ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoCollapse) )
+		if ( ImGui::Begin( "Lazysight", nullptr, ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoCollapse ) )
 		{
-			ImGui::Text("Hello, world :p");
+			ImGui::Text( "Hello, world :p" );
 			ImGui::End();
 		}
 	}
 
 	ImGui::EndFrame();
 	ImGui::Render();
-	ImGui_ImplDX9_RenderDrawData(ImGui::GetDrawData());
+	ImGui_ImplDX9_RenderDrawData( ImGui::GetDrawData() );
 
-	return o_present( p_device , p_src_rect , p_dest_rect , h_dest_window , p_dirty_region );
+	return o_present( p_device, p_src_rect, p_dest_rect, h_dest_window, p_dirty_region );
 }
