87babeb2586c247b38e1575d861dbedf13f890b7
LuanDevecchi <gitluan@engineer.com>
2020-01-23 23:44:00 -0300

    fix duplicate class

--- ironsight/weapon_mgr.h
+++ ironsight/weapon_mgr.h
@@ -20,15 +20,3 @@ public:
 	uint32_t m_slot1_ammo; //0x040C
 	uint32_t m_slot1_ammo2; //0x0410
 }; //Size: 0x0414
-
-class CWeaponInfo
-{
-public:
-	char pad_0000[60]; //0x0000
-	float m_max_increase_recoil; //0x003C
-	char pad_0040[36]; //0x0040
-	float m_recoil; //0x0064
-	float m_recoil_tick; //0x0068
-	char pad_006C[4]; //0x006C
-	float m_nospread; //0x0070
-}; //Size: 0x0074
